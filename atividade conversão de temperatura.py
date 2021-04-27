75788e7c807bf9320719b98500dfe7a24f455984
Allandelon Arruda <73965188+Fawkes0511@users.noreply.github.com>
2021-04-27 00:13:07 -0300

    Atividade pedida pelo professor da UFRPE.
    código simples para conversão de temperatura, estudar a lógica de programação ajudou bastante.

---
+++ atividade conversão de temperatura.py
@@ -0,0 +1,18 @@
+# Programa para inserir cidade, tenperatura e escala.
+cidade = input('Digite sua cidade: ').strip()
+temperatura = float(input('Digite a temperatura da sua cidade: '))
+escala = input('Digite a escala.ex(K,C OU F): ')
+
+if escala == 'F':
+    C = (temperatura - 32) * (5 / 9)
+elif escala == 'K':
+    C = (temperatura - 273.15)
+else:
+    C = temperatura
+if C < 17:
+    temperatura = 'fria'
+elif C < 30:
+    temperatura = 'ambiente'
+else:
+    temperatura = 'quente'
+print('{} está {} e medindo {:.1f}°C'.format(cidade, temperatura, C))
