103abe2059d2bcd47214df99eac416c5d8ab3999
Allandelon Arruda <73965188+Fawkes0511@users.noreply.github.com>
2021-04-26 22:38:01 -0300

    Atividade pedida pelo professor da UFRPE.
    Calculadora simples.

--- atividade calculadora.py
+++ atividade calculadora.py
@@ -1,4 +1,4 @@
-# Atividade para criar um programa que simula uma calculadora simples.
+# Atividade para criar um programa que simula uma calculadora simples. 
 
 N1_op_N2 = input('Digite seu cálculo,ex(2 * 2): ').split()
 N1, opera, N2 = int(N1_op_N2[0]), N1_op_N2[1], int(N1_op_N2[2])
